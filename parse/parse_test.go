159b6ea75a619d87b7e6690a6fa8f4c29aa15e84
Javi Fontan <jfontan@gmail.com>
2018-01-22 20:48:52 +0100

    Fix parse testing

--- parse/parse_test.go
+++ parse/parse_test.go
@@ -1,10 +1,7 @@
 package parse
 
 import (
-	"bufio"
-	"fmt"
 	"reflect"
-	"strings"
 	"testing"
 )
 
@@ -20,7 +17,7 @@ t=2017-11-14t23:10:03+0000 lvl=info msg="job started" module=borges worker=2 job
 
 	examples = map[string]map[string]string{
 		`t=2017-11-14t23:10:03+0000 lvl=info msg="initializing pack process" module=borges file=repos.txt output=repositories caller=packer.go:34`: {
-			"t":      "2017-11-14T23:10:03+0000",
+			"t":      "2017-11-14t23:10:03+0000",
 			"lvl":    "info",
 			"msg":    "initializing pack process",
 			"module": "borges",
@@ -29,16 +26,44 @@ t=2017-11-14t23:10:03+0000 lvl=info msg="job started" module=borges worker=2 job
 			"caller": "packer.go:34",
 		},
 		`t=2017-11-14t23:10:03+0000 lvl=info msg=starting module=borges worker=1 caller=worker.go:37`: {
-			"t":      "2017-11-14T23:10:03+0000",
+			"t":      "2017-11-14t23:10:03+0000",
 			"lvl":    "info",
-			"msg":    "initializing pack process",
+			"msg":    "starting",
 			"module": "borges",
-			"file":   "repos.txt",
-			"output": "repositories",
-			"caller": "packer.go:34",
+			"worker": "1",
+			"caller": "worker.go:37",
 		},
 	}
 )
+
+// mapEqual can be used to pinpoint what is different
+func mapEqual(a, b map[string]string) bool {
+	if a == nil && b == nil {
+		println("nil nil")
+		return true
+	} else if a == nil || b == nil {
+		println("one nil")
+		return false
+	}
+
+	aLen := len(a)
+	bLen := len(b)
+
+	if aLen != bLen {
+		println("len")
+		return false
+	}
+
+	for k, v := range a {
+		if bV, ok := b[k]; !ok || v != bV {
+			println(k, v, bV)
+			println("key differs", k, v, bV)
+			return false
+		}
+	}
+
+	return true
+}
 
 func TestExamples(t *testing.T) {
 	for k, v := range examples {
@@ -48,23 +73,4 @@ func TestExamples(t *testing.T) {
 			t.Errorf("Result does not match for %q.\n  Expected: %q\n  Got:      %q", k, v, res)
 		}
 	}
-}
-
-func ExampleCorrect() {
-	reader := strings.NewReader(text)
-	scanner := bufio.NewScanner(reader)
-
-	for scanner.Scan() {
-		m := ParseLine(scanner.Text())
-		fmt.Printf("%q\n", m)
-	}
-	// ttt
-	// map["t":"2017-11-14T23:10:03+0000" "lvl":"info" "msg":"initializing pack process" "module":"borges" "file":"repos.txt" "output":"repositories" "caller":"packer.go:34"]
-	// map["worker":"1" "caller":"worker.go:37" "t":"2017-11-14T23:10:03+0000" "lvl":"info" "msg":"starting" "module":"borges"]
-	// map["t":"2017-11-14T23:10:03+0000" "lvl":"info" "msg":"starting" "module":"borges" "worker":"0" "caller":"worker.go:37"]
-	// map["t":"2017-11-14T23:10:03+0000" "lvl":"info" "msg":"starting" "module":"borges" "worker":"2" "caller":"worker.go:37"]
-	// map["lvl":"info" "msg":"starting" "module":"borges" "worker":"3" "caller":"worker.go:37" "t":"2017-11-14T23:10:03+0000"]
-	// map["module":"borges" "worker":"1" "job":"015fbccb-f1b2-3278-d997-038f7156d8b7" "caller":"archiver.go:76" "t":"2017-11-14T23:10:03+0000" "lvl":"info" "msg":"job started"]
-	// map["module":"borges" "worker":"0" "job":"015fbccb-f1b2-aff2-5d04-868e93735dfd" "caller":"archiver.go:76" "t":"2017-11-14T23:10:03+0000" "lvl":"info" "msg":"job started"]
-	// map["module":"borges" "worker":"2" "job":"015fbccb-f1b2-2903-3aac-4eafc2196a7d" "caller":"archiver.go:76" "t":"2017-11-14T23:10:03+0000" "lvl":"info" "msg":"job started"]
 }

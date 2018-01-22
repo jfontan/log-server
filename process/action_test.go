9e9b6b7975b61f7c0547ec2cc5ba7e0717fd6691
Javi Fontan <jfontan@gmail.com>
2018-01-22 22:54:43 +0100

    Add action functions tests

--- process/action_test.go
+++ process/action_test.go
@@ -67,3 +67,91 @@ func TestStartAction(t *testing.T) {
 		t.Error("Event was not modified")
 	}
 }
+
+func TestRegexpFilter(t *testing.T) {
+	filter := map[string]string{
+		"one": "one",
+		"two": "^two",
+	}
+
+	cases := []struct {
+		Event *Event
+		Nil   bool
+	}{
+		{
+			Event: &Event{
+				Data: map[string]string{
+					"one": "one",
+				},
+			},
+			Nil: true,
+		},
+		{
+			Event: &Event{
+				Data: map[string]string{
+					"one": "one",
+					"two": "two",
+				},
+			},
+			Nil: false,
+		},
+		{
+			Event: &Event{
+				Data: map[string]string{
+					"two": "two",
+				},
+			},
+			Nil: true,
+		},
+	}
+
+	f := GenRegexpFilter(filter)
+
+	for _, test := range cases {
+		res := f(test.Event)
+
+		if (res == nil) != test.Nil {
+			if test.Nil {
+				t.Errorf("The event %v should not past the test", test.Event)
+			} else {
+				t.Errorf("The event %v should past the test", test.Event)
+			}
+		}
+	}
+}
+
+func testCounter(t *testing.T, name string, expected uint) {
+	val, ok := counters[name]
+
+	if !ok {
+		t.Errorf("Counter %v should be defined", name)
+	}
+
+	if val != expected {
+		t.Errorf("Counter %v has value %v, is expected to be %v",
+			name, val, expected)
+	}
+}
+
+func TestCounterAction(t *testing.T) {
+	if counters != nil {
+		t.Fatal("Counter must be nil when the test starts")
+	}
+
+	counterA := GenCounterAction("a")
+	counterB := GenCounterAction("b")
+
+	event := &Event{
+		Origin: "testing",
+	}
+
+	if counterA(event) != event {
+		t.Error("Counter function should return the same event")
+	}
+
+	counterA(nil)
+	counterB(nil)
+
+	testCounter(t, "a", 2)
+	testCounter(t, "b", 1)
+}
